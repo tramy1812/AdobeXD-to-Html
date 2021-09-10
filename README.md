5df7564cab046cf1194fcea573d3695ef20cb82b
tramy1812 <dangtramy.1812@gmail.com>
2021-09-10 11:30:32 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AdobeXD-to-Html
+
+# Mobile responsive html
+
+XD Files: https://xd.adobe.com/view/e352f831-d790-4f96-8fa0-6ecf83330699-b601/
---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.0 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    // if (mainNav) {
+    //     new bootstrap.ScrollSpy(document.body, {
+    //         target: '#mainNav',
+    //         offset: 72,
+    //     });
+    // };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// Developed at agap2
+// Based on:
+// http://www.codeply.com/go/s3I9ivCBYH/multi-carousel-single-slide-bootstrap-4
+
+var swiper = new Swiper(".swiper-container", {
+  loopedSlides: 8,
+  loop: false,
+  slidesPerView: "auto",
+  freeMode: true,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+});
